adba033440a87d74020eb21dd64a02ecc96a6b8a
Alex-A-B <alexjamesbelton@gmail.com>
2021-07-16 22:21:19 +0100

    added the horizontal rule

--- src/components/DisplayComments.js
+++ src/components/DisplayComments.js
@@ -3,6 +3,7 @@ import CommentItem from "./CommentItem";
 function DisplayComments( { comments }) {
     return (
         <div className="comments">
+            <hr width="75%" align="center" />
             <h2>{comments.length} Comments</h2>
             {comments.map((comment) => <CommentItem key={comment.id} user={comment.user} text={comment.comment}/>)}
         </div>
